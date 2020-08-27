870538da5d3f290abffcb01ae2cf34c06ef8da80
Ricardo Salveti <ricardo@foundries.io>
2020-08-27 11:09:26 -0300

    [FIO extras] imx8mm_evk: disable default hardcoded bootcmd
    Allow bootcmd to be customized via CONFIG_BOOTCOMMAND.
    
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

--- include/configs/imx8mm_evk.h
+++ include/configs/imx8mm_evk.h
@@ -171,17 +171,6 @@
 			"fi; " \
 		"fi;\0"
 
-#define CONFIG_BOOTCOMMAND \
-	   "mmc dev ${mmcdev}; if mmc rescan; then " \
-		   "if run loadbootscript; then " \
-			   "run bootscript; " \
-		   "else " \
-			   "if run loadimage; then " \
-				   "run mmcboot; " \
-			   "else run netboot; " \
-			   "fi; " \
-		   "fi; " \
-	   "fi;"
 #endif
 
 /* Link Definitions */
