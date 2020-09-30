b9e57dd83c19833c3233fdf304ab12749cc2b280
Igor Opaniuk <igor.opaniuk@toradex.com>
2020-09-30 16:42:38 +0300

    [FIO toup] usb: gadget: ci_udc: introduce board_ci_udc_phy_mode
    Introduce board_ci_udc_phy_mode() weak function which provides
    opportunity to re-define the logic of OTG role detection, and use
    board specific way of generation of USB cable states (for example,
    when USB ID pin is connected to a GPIO pin).
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@toradex.com>
    Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

--- drivers/usb/gadget/ci_udc.c
+++ drivers/usb/gadget/ci_udc.c
@@ -1313,17 +1313,14 @@ static int ci_udc_otg_clk_init(struct udevice *dev,
 	return 0;
 }
 
-static int ci_udc_otg_phy_mode(struct udevice *dev)
+int __weak board_ci_udc_phy_mode(void *__iomem phy_base, int phy_off)
 {
-	struct ci_udc_priv_data *priv = dev_get_priv(dev);
-
 	void *__iomem phy_ctrl, *__iomem phy_status;
-	void *__iomem phy_base = (void *__iomem)devfdt_get_addr(&priv->otgdev);
 	u32 val;
 
 	if (is_mx6() || is_mx7ulp() || is_imx8()) {
 		phy_base = (void __iomem *)fdtdec_get_addr(gd->fdt_blob,
-							   priv->phy_off,
+							   phy_off,
 							   "reg");
 		if ((fdt_addr_t)phy_base == FDT_ADDR_T_NONE)
 			return -EINVAL;
@@ -1345,6 +1342,15 @@ static int ci_udc_otg_phy_mode(struct udevice *dev)
 	} else {
 		return -EINVAL;
 	}
+}
+
+
+static int ci_udc_otg_phy_mode(struct udevice *dev)
+{
+	struct ci_udc_priv_data *priv = dev_get_priv(dev);
+
+	void *__iomem phy_base = (void *__iomem)devfdt_get_addr(&priv->otgdev);
+	return board_ci_udc_phy_mode(phy_base, priv->phy_off);
 }
 
 static int ci_udc_otg_ofdata_to_platdata(struct udevice *dev)
