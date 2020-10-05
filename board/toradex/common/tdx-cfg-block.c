7780d4ec8ee3b8c6b0f520f4a01b44f939f7335a
Marcel Ziswiler <marcel.ziswiler@toradex.com>
2020-10-05 02:26:46 +0200

    toradex: tdx-cfg-clock: add new i.mx 8m mini/plus skus
    Add new i.MX 8M Mini/Plus SKUs to ConfigBlock handling:
    
    0058: Verdin iMX8M Plus Quad 4GB Wi-Fi / BT IT
    
    0059: Verdin iMX8M Mini Quad 2GB IT
    
    0060: Verdin iMX8M Mini DualLite 1GB WB IT
    
    0061: Verdin iMX8M Plus Quad 2GB
    
    Related-to: ELB-3128
    Signed-off-by: Marcel Ziswiler <marcel.ziswiler@toradex.com>

--- board/toradex/common/tdx-cfg-block.c
+++ board/toradex/common/tdx-cfg-block.c
@@ -16,7 +16,8 @@
 	defined(CONFIG_TARGET_COLIBRI_IMX6) || \
 	defined(CONFIG_TARGET_COLIBRI_IMX8X) || \
 	defined(CONFIG_TARGET_VERDIN_IMX8MM) || \
-	defined(CONFIG_TARGET_VERDIN_IMX8MN)
+	defined(CONFIG_TARGET_VERDIN_IMX8MN) || \
+	defined(CONFIG_TARGET_VERDIN_IMX8MP)
 #include <asm/arch/sys_proto.h>
 #else
 #define is_cpu_type(cpu) (0)
@@ -139,6 +140,10 @@ const char * const toradex_modules[] = {
 	[55] = "Verdin iMX8M Mini Quad 2GB Wi-Fi / BT IT",
 	[56] = "Verdin iMX8M Nano Quad 1GB Wi-Fi / BT", /* not currently on sale */
 	[57] = "Verdin iMX8M Mini DualLite 1GB",
+	[58] = "Verdin iMX8M Plus Quad 4GB Wi-Fi / BT IT",
+	[59] = "Verdin iMX8M Mini Quad 2GB IT",
+	[60] = "Verdin iMX8M Mini DualLite 1GB WB IT",
+	[61] = "Verdin iMX8M Plus Quad 2GB",
 };
 
 const char * const toradex_carrier_boards[] = {
@@ -361,21 +366,15 @@ static int get_cfgblock_interactive(void)
 
 	if (cpu_is_pxa27x())
 		sprintf(message, "Is the module the 312 MHz version? [y/N] ");
-#if !defined(CONFIG_TARGET_VERDIN_IMX8MM) || !defined(CONFIG_TARGET_VERDIN_IMX8MN)
-	else
-		sprintf(message, "Is the module an IT version? [y/N] ");
-
-	len = cli_readline(message);
-	it = console_buffer[0];
-#else
 	else
 		it = 'y';
-#endif
 
 #if defined(CONFIG_TARGET_APALIS_IMX8) || \
 		defined(CONFIG_TARGET_APALIS_IMX8X) || \
 		defined(CONFIG_TARGET_COLIBRI_IMX6ULL) || \
-		defined(CONFIG_TARGET_COLIBRI_IMX8X)
+		defined(CONFIG_TARGET_COLIBRI_IMX8X) || \
+		defined(CONFIG_TARGET_VERDIN_IMX8MM) || \
+		defined(CONFIG_TARGET_VERDIN_IMX8MP)
 	sprintf(message, "Does the module have Wi-Fi / Bluetooth? [y/N] ");
 	len = cli_readline(message);
 	wb = console_buffer[0];
@@ -465,6 +464,25 @@ static int get_cfgblock_interactive(void)
 				tdx_hw_tag.prodid = COLIBRI_IMX8DX;
 		}
 #endif
+	} else if (is_cpu_type(MXC_CPU_IMX8MM)) {
+		if (is_cpu_type(MXC_CPU_IMX8MMDL)) {
+			if (wb == 'y' || wb == 'Y')
+				tdx_hw_tag.prodid = VERDIN_IMX8MMDL_WIFI_BT_IT;
+			else
+				tdx_hw_tag.prodid = VERDIN_IMX8MMDL;
+		} else {
+			if (wb == 'y' || wb == 'Y')
+				tdx_hw_tag.prodid = VERDIN_IMX8MMQ_WIFI_BT_IT;
+			else
+				tdx_hw_tag.prodid = VERDIN_IMX8MMQ_IT;
+		}
+	} else if (is_cpu_type(MXC_CPU_IMX8MN)) {
+		tdx_hw_tag.prodid = VERDIN_IMX8MNQ_WIFI_BT;
+	} else if (is_cpu_type(MXC_CPU_IMX8MP)) {
+		if (wb == 'y' || wb == 'Y')
+			tdx_hw_tag.prodid = VERDIN_IMX8MPQ_WIFI_BT_IT;
+		else
+			tdx_hw_tag.prodid = VERDIN_IMX8MPQ;
 	} else if (!strcmp("tegra20", soc)) {
 		if (it == 'y' || it == 'Y')
 			if (gd->ram_size == 0x10000000)
