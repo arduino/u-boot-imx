2a76741bb2e28a892a58661ac465cc6909407e56
Michael Scott <mike@foundries.io>
2020-10-08 16:04:20 -0700

    [FIO extras] imx8mq_evk: allow hardcoded bootcmd to be overridden
    Allow bootcmd to be customized via CONFIG_BOOTCOMMAND in defconfig
    
    Signed-off-by: Michael Scott <mike@foundries.io>

--- include/configs/imx8mq_evk.h
+++ include/configs/imx8mq_evk.h
@@ -134,6 +134,7 @@
 			"booti; " \
 		"fi;\0"
 
+#ifndef CONFIG_BOOTCOMMAND
 #define CONFIG_BOOTCOMMAND \
 	   "mmc dev ${mmcdev}; if mmc rescan; then " \
 		   "if run loadbootscript; then " \
@@ -145,6 +146,7 @@
 			   "fi; " \
 		   "fi; " \
 	   "else booti ${loadaddr} - ${fdt_addr}; fi"
+#endif
 
 /* Link Definitions */
 #define CONFIG_LOADADDR			0x40480000
