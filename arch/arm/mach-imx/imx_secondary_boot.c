00ed6fa41f50bb1de5e7ef555daa168d1346416a
Igor Opaniuk <igor.opaniuk@foundries.io>
2021-03-10 12:22:20 +0200

    [FIO internal] imx: add secondary_boot command
    Add command for triggering SiP service provided by TF-A for setting/clearing
    SRC GPR10 PERSIST_SECONDARY_BOOT bit, that identifies which image will be
    used for boot after warm reset: primary(0) or secondary(1).
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>
    Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

---
+++ arch/arm/mach-imx/imx_secondary_boot.c
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2021 Foundries.io Ltd
+ */
+
+#include <common.h>
+#include <asm/io.h>
+#include <asm/mach-imx/sys_proto.h>
+#include <command.h>
+#include <elf.h>
+#include <imx_sip.h>
+#include <linux/arm-smccc.h>
+#include <linux/compiler.h>
+#include <cpu_func.h>
+
+static int do_imx_secondary_boot(cmd_tbl_t *cmdtp, int flag,
+				 int argc, char * const argv[])
+{
+	u32 secondary = 0;
+	struct arm_smccc_res res;
+	char secondary_val[2] = { 0 };
+	int ret;
+
+	/* If we just want to retrieve current value */
+	if (argc == 1) {
+		arm_smccc_smc(IMX_SIP_SRC, IMX_SIP_SRC_IS_SECONDARY_BOOT, 0, 0,
+			      0, 0, 0, 0, &res);
+
+		secondary = res.a0;
+		printf("Secondary boot bit = %d\n", secondary);
+		ret = snprintf(secondary_val, sizeof(secondary_val),
+			       "%d", secondary);
+		if (ret < 0)
+			return CMD_RET_FAILURE;
+
+		ret = env_set("fiovb.is_secondary_boot", secondary_val);
+		if (ret)
+			return CMD_RET_FAILURE;
+
+		return CMD_RET_SUCCESS;
+	}
+
+	secondary = simple_strtoul(argv[1], NULL, 10);
+
+	if (!(secondary == 0 || secondary == 1))
+		return CMD_RET_USAGE;
+
+	arm_smccc_smc(IMX_SIP_SRC, IMX_SIP_SRC_SET_SECONDARY_BOOT, secondary, 0,
+		      0, 0, 0, 0, &res);
+
+	return CMD_RET_SUCCESS;
+}
+
+U_BOOT_CMD(
+	imx_secondary_boot, CONFIG_SYS_MAXARGS, 1, do_imx_secondary_boot,
+	"Get/Set PERSIST_SECONDARY_BOOT bit",
+	"[0|1]\n"
+	"   no param  - get current bit value\n"
+	"   0 - set primary image\n"
+	"   1 - set secondary image\n"
+);
